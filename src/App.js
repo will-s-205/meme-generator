938dbba99ccbcd7d24380db95636834b83499ee1
William S <will.step.205@gmail.com>
2023-06-17 14:47:56 -0700

    navbar adjustments

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
