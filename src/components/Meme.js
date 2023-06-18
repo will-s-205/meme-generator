817876957dee2c994c62bb597e72c013954f3085
William S <will.step.205@gmail.com>
2023-06-18 11:58:29 -0700

    read random url

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,15 +1,22 @@
+import memesData from "../data/memesData"
+import { random } from 'lodash';
+
 export default function Navbar() {
+    const handleClick = () => {
+        // console.log(memesData.data.memes[0].url)
+        // console.log({ url: memesData.data.memes[0].url })
+        const randomUrl = memesData.data.memes[random(0, memesData.data.memes.length)];
+        console.log(randomUrl.url)
+    }
     return (
-        <div>
-            <main>
-                <form action="" className="input-form">
-                    <div className="input-fields">
-                        <input className="input-top" placeholder="top text"></input>
-                        <input className="input-bottom" placeholder="bottom text"></input>
-                    </div>
-                    <input className="submit-button" type="submit" value="Get a new meme image"></input>
-                </form>
-            </main>
-        </div>
+        <main>
+            <form action="" className="input-form">
+                <div className="input-fields">
+                    <input className="input-top" placeholder="top text"></input>
+                    <input className="input-bottom" placeholder="bottom text"></input>
+                </div>
+            </form>
+            <button onClick={handleClick} className="submit-button">Get a new meme image</button>
+        </main>
     )
 }
