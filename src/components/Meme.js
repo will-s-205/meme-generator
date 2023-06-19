87bee0171c2af5f18ea0b2d5e44d96b815b807eb
William S <will.step.205@gmail.com>
2023-06-19 12:18:03 -0700

    download meme functionality added

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,7 +1,58 @@
-import { useState } from "react";
+import { createRef, useState } from "react";
+import { createFileName, useScreenshot } from 'use-react-screenshot'
 import memesData from "../data/memesData"
+import html2canvas from "html2canvas";
 
 export default function Navbar() {
+
+    html2canvas(document.getElementById("meme"), { logging: true, letterRendering: 1, allowTaint: false, useCORS: true }).then(canvas => { })
+
+    const ref = createRef(null)
+    const [image, takeScreenshot] = useScreenshot({
+        type: "image/jpg",
+        quality: 1.0
+    })
+
+    const download = (image, { name = "meme", extension = "jpg" } = {}) => {
+        const a = document.createElement("a");
+        a.href = image;
+        a.download = createFileName(extension, name);
+        a.click();
+    };
+
+    const downloadScreenshot = () => takeScreenshot(ref.current).then(download);
+
+    const downloadScreenshot2 = () => {
+        return html2canvas(document.getElementsByClassName('meme-img')).then(function (canvas) {
+            document.getElementsByClassName('meme-img').src = canvas.toDataURL();
+        });
+    }
+
+    const one = () => {
+        
+        html2canvas(document.getElementById("meme"), {
+            letterRendering: 1,
+            logging: true,
+            letterRendering: 1,
+            useCORS: true,
+        }).then(function (canvas) {
+            var myImage = canvas.toDataURL();
+            downloadURI(myImage, "promo.png");
+        });
+
+        function downloadURI(uri, name) {
+            var link = document.createElement("a");
+
+            link.download = name;
+            link.href = uri;
+            document.body.appendChild(link);
+            link.click();
+            //after creating link you should delete dynamic link
+        }
+    }
+
+
+
     const [memeImg, setMemeImg] = useState('https://i.imgflip.com/odluv.jpg')
 
     const GetMemeImg = () => {
@@ -15,6 +66,28 @@ export default function Navbar() {
 
     return (
         <main>
+
+            <div>
+                <button onClick={downloadScreenshot}>Download screenshot</button>
+                <div
+                    ref={ref}
+                    style={{
+                        border: "1px solid #ccc",
+                        background: 'red',
+                        padding: "10px",
+                        marginTop: "20px"
+                    }}
+                >
+                    <h1>John Williamson is the Captain<br></br>
+                        of New Zealand
+                    </h1>.
+                </div>
+            </div>
+
+
+
+
+
             <form action="" className="input-form">
                 <div className="input-fields">
                     <input className="input-top" placeholder="top text"></input>
@@ -22,7 +95,10 @@ export default function Navbar() {
                 </div>
             </form>
             <button onClick={GetMemeImg} className="submit-button">Get a new meme image</button>
-            <img src={memeImg} className="meme-img" alt="meme img" />
+            <button onClick={one} className="submit-button">Download</button>
+            <div ref={ref} id='meme'>
+                <img crossorigin="anonymous" src={memeImg} className="meme-img" alt="meme img" />
+            </div>
         </main>
     )
 }
