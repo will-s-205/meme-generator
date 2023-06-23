6fd873fb073a71b166e0be85bc0bfd16480506f1
William S <will.step.205@gmail.com>
2023-06-23 14:44:09 -0700

    border radius added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,10 @@ import troll_logo from '../images/troll-face-small.png'
 
 export default function Navbar() {
     return (
-        <nav>
+        <header>
             <img src={troll_logo} alt="troll logo"/>
             <h1>Meme Generator</h1>
             <p>Click image below</p>
-        </nav>
+        </header>
     )
 }
